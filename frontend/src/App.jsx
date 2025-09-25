5ddb95fa12f72854848a6ddc26cb38c397787abd
Anand maurya <iliveinindia134@gmail.com>
2025-09-25 13:58:50 +0530

    adding frontend and backend part

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Hello Tailwind + Vite 🎉</h1>
+    </>
+  )
+}
+
+export default App
